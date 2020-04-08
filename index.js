a94f50903e9948c52411180baa03e6d427e6858e
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-08 17:46:16 -0500

    add examples of 'const' and let

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//primitives CANNOT be Mutated
+//const CANNOT be reassigned
+//objects CAN be mutated
+//only use "let" when you have to reassign something
+
+
+const name = "dye";
+
+const color = "brown";
+
+const number = 19;
+
+let x = 17;
+
+luckyNumber = 17;
+//JS is a DYNAMICALLY typed language.
+
+//"constant" is consistent."let" can be changed.
+//everything in java is an object except primitive.
+
+const myObject = {};
+myObject.name = "Dyeshia";
+
+git add package-lock-json
+git status
