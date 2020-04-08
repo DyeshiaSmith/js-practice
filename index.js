fbeb4ed6c91644e4da5d85632372ed997bab0336
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-08 18:54:11 -0500

    Add weird examples of how to create common errors.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 //primitives CANNOT be Mutated
 //const CANNOT be reassigned
 //objects CAN be mutated
 //only use "let" when you have to reassign something
-
 
 const name = "dye";
 
@@ -12,6 +10,8 @@ const color = "brown";
 const number = 19;
 
 let x = 17;
+
+x =23;
 
 luckyNumber = 17;
 //JS is a DYNAMICALLY typed language.
@@ -22,5 +22,43 @@ luckyNumber = 17;
 const myObject = {};
 myObject.name = "Dyeshia";
 
-git add package-lock-json
-git status
+git commit -am (shortcut) "add examples for comments with objects, primitives etc"
+
+
+myObject.pet = "Dog"
+console.log(myObject.pet);
+
+//UNDEFINED and NULL are there own PRIMITIVE DATA TYPES
+//UNDEFINED
+//with CONST must assign value
+//CONST CANNOT be reassigned or mutated
+
+const myObject = {
+  age: 23
+}
+//DOT NOTATION says "so inside this object and look for key with some name. Just tell me the value"
+console.log(myObject.age, myObject.name);
+
+myObject.wrestler = "undertaker"
+
+console.log(myObject);
+//JS shows 'undefined' if we access a key that doesn't exist in an OBJECT.
+console.log(myObject.somethingElse);
+
+//chaining
+//console.log(me.kids.older)
+const me = {
+  name:"dye",
+  kids: {
+    older: "uma"
+    younger: "lily"
+  }
+}
+console.log(me.somethingElse); -undefined
+console.log(me.somethingElse.again); -undefined/chaining
+console.log(undefined.again); -
+
+//console.log(console);
+
+
+
