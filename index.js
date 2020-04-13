18cddc1f8e9215bf89b7e1fa5ea056503019a6cf
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-13 17:36:24 -0500

    Logical/Comparision operators

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const age = 25;
-
-// If age is greater than or equal to 25...
-if (age >= 25) {
-  console.log("Renting a car is more affordable.")
-} else {
-  // else: age is less than 25
-  console.log("Renting a car is very expensive.")
+const num = 8;
+//if the #mis greater than 100 or less than0
+if (num > 100 || num < 0) {
+  console.log("The number is greater than 100 or less than 0,");
 }
 
-// chaining if statements: if - else if - ... - else
-// if age is greater than or equal to 25,
-if (age >= 25) {
-  console.log("Renting a car is more affordable.")
-} else if (age >=18) {
-  // else: age is less than 25
-  // if age is greater than or equal to 18
-  console.log("Renting a car is very expensive.")
-} else {
-  // else: age is less than 18
-  console.log("You cannot legally rent a car.")
+//if the number is less than 100 AND greater than 0.
+if (num > 100 && num < 0) {
+  console.log("the number is greater than 100 and less than 0.");
 }
 
-const x = 2;
-
-// if (some comparison operator in parentheses is true)...
-if (x === 3) {
-		console.log("Since, the if condition above is true, you're seeing this on the screen.");
-} else {
-	console.log("Nope, you can't see me!");
+//TODO: combine the AND and OR with and 'if" - make up your own
+if (num > 1 && num === 8) {
+  console.log("you pass");
 }
-//OPERATORS AND IF
-console.log("this will always run (in no errors)");
- if ("x >21") {
-   console.log("Can buy alcohol.");
- } else if ("x <21"){
-   console.log("Cannot buy alcohol.");
- else if (x > 25 "Can can buy alcohol");
- }
+//his example
+if ((num > 45 && num < 50) || num === 99) {
+  console.log("got an A");
+}
+// Logical Operators
+console.log(true && false); //true and false, b/c youll end up eith false
+console.log(true || false); //true OR False. only left side will be evaluated
+console.log(!true); // not true. False
+console.log(!(true && false)); // not false. which means True
+console.log(false || !false); // false // true which means TRUE.
+
+// Logical Operators and Truthy/Falsy
+// ? what do you expect each line to print?
+console.log("we are here" && 0); // false && false
+console.log(undefined || null); // false or false
+console.log(!``); // true. (b/c empty strings and 0 means falsey)
+console.log(!(1 && "false"));
+console.log(NaN || !"true");
+
+// TODO: using only logical operators (no comparison operators), write an if statement that prints "The number is zero" when num is 0.
+//let num = 0;
+// * your if statement here *
+
+{
+}
