2d3b9ec9ffc42e2c756c558b39ee85a3c52df2df
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-13 19:01:54 -0500

    loops 1 using while and for

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-let myCar = "Mazda";
+//log #s from 1 to 10
+let currentNum = 1;
+let total = 0;
+while (currentNum < 100) {
+  total += currentNum; // total + total + currentNum
+  currentNum = currentNum + 2; //currentNum +=2
+  console.log(total);
+}
 
-/**
- * Let's get 'you' the 'same' car as me.
- * Since we are working with PRIMITIVES,
- * this means that 'your car' will be a separate 'copy' of mine.
- */
-let yourCar = myCar; // 'you' have your own 'Mazda.'
+let currentTotal = 0;
 
-// I am replacing my car.
-myCar = "Ford";
+// for (some initial condition; some ending condition; some increment interval) {
+for (let currentNum = 1; currentNum <= 10; currentNum += 1) {
+  currentTotal += currentNum;
+}
 
-console.log(myCar); // 'Ford'
+console.log(currentTotal);
+// TODO: Update code to count by 2s and add the numbers from 1 to 100.
 
-// Your car is the same
-console.log(yourCar); // 'Mazda'
+// TODO: Use a 'while' loop to log the numbers from 1 to a 100.
+// However, if the number is divisible by '3,' log 'Fizz!'. If it' divisible by '5,' log 'Buzz!'.
+// If it's d(ivisible by both 3 AND 5, 'log' 'Fizz!Buzz!'. hint log===0, use mod operator [if)]
+console.log();
