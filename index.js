3ad0f90c1bf2a61fe07563231092dccc58b1fec7
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-14 16:51:38 -0500

    Truthy and falsey with && ||

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-//log #s from 1 to 10
-let currentNum = 1;
-let total = 0;
-while (currentNum < 100) {
-  total += currentNum; // total + total + currentNum
-  currentNum = currentNum + 2; //currentNum +=2
-  console.log(total);
-}
+// 'or' Short Circuit! - "a" is 'truthy', so no need to for '333'
+console.log("a" || 333); // a
 
-let currentTotal = 0;
+const a = "";
 
-// for (some initial condition; some ending condition; some increment interval) {
-for (let currentNum = 1; currentNum <= 10; currentNum += 1) {
-  currentTotal += currentNum;
-}
+// 'a' is bound to a 'falsey' empty STRING, so we 'take' 'truthy' '333'.
+a || 333; // 333
 
-console.log(currentTotal);
-// TODO: Update code to count by 2s and add the numbers from 1 to 100.
+null || "hello"; // hello
+
+// "a" is 'truthy,' but still must look at '333'
+"a" && 333; // 333
+
+// 'and' Short Circuit! - is 'falsey' - don't bother with '333'.
+0 && 333; // 0
+
+// 'and' Short Circuit! - 'null 'falsey' - don't bother with 'hello'.
+console.log(null && "hello"); // null
+
+// Imagine user left form field blank - no FORM VALIDATION.
+// We can assign a default value so that we can still 'accept' the 'form,' but it's clear that the user left that blank.
+const userName = "" || "Blank form field"; //
+
+// Imagine that we retrieved some product from database as an OBJECT LITERAL.
+const someProduct = {
+  name: "Great Widget",
+  price: 0 // Bad data entry - no database validation! 😱
+};
+
+// We can prevent '0' from being shown to user.
+const displayedPrice = someProduct.price || "Please Call For Price!";
