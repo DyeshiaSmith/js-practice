4aa66a313e4b3bd2f725e9beedc9db91c79fa1a6
Dyeshia Smith <dyeshiasmith@Dyeshias-MBP.lan>
2020-04-14 17:56:32 -0500

    get example of rest peramiters with looping array

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// Global scop not enclosed with any '{}'
-const x = 3;
+//variadic Named Parameters
+// ... REST PARAMETER wraps up VARIADIC NUMBER OF ARGUMENTS
 
-const dye = function() {
-  //if we declare a variable inside a scope.. its NOT available globally- LOCAL SCOPE || FUNCTION SCOPE
-  const y = 4;
-  console.log("practicing logging this string and learning functions.");
+const dye = function(...hola) {
+  console.log(hola);
+
+  for (let i = 0; i < hola.length; i++) {
+    console.log(hola[i]);
+  }
 };
 
-dye();
+dye("hola", "hey", "hi", "hoy", "sup");
+console.log("hola", "hey", "hi", "hoy", "sup");
+console.log("hey");
 
-console.log(dye);
+//write a loop to log out each of the greetings 1 at a time
